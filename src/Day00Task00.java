1ccbdb70fd3f910e338f2d77a0bc0921874e2041
Vladimir Iotchenko <mojmail@inbox.lv>
2022-02-20 23:02:03 +0200

    File from DayZero

---
+++ src/Day00Task00.java
@@ -0,0 +1,38 @@
+public class Day00Task00 {
+
+    public static void main(String[] args) {
+
+        // Declaring variables
+
+        byte tiny;
+        short small;
+        int number;
+        long bigNumber;
+        float point;
+        double comma;
+        char c;
+        boolean isOK;
+
+        // Assigning values
+
+        tiny = 127;
+        small = 512;
+        number = 35000;
+        bigNumber = 125000000;
+        point = 1.215f;
+        comma = 3.141819202122;
+        c = 'Z';
+        isOK = true;
+
+        // Printing it all out
+
+        System.out.println("byte is " + tiny);
+        System.out.println("short is " + small);
+        System.out.println("int is " + number);
+        System.out.println("long is " + bigNumber);
+        System.out.println("float is " + point);
+        System.out.println("double is " + comma);
+        System.out.println("char is " + c);
+        System.out.println("boolean is " + isOK);
+    }
+}
